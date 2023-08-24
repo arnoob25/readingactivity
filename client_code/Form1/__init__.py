542a604a450e690526c6410aec9cda09e416165a
muhammadarnobshikder@gmail.com <muhammadarnobshikder@gmail.com>
2023-08-24 11:40:36 +0000

    Edited Form1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -12,7 +12,7 @@ class Form1(Form1Template):
 
     # Any code you write here will run before the form opens.
 
-  def button_1_click(self, **event_args):
+  def btn_outline(self, **event_args):
     """This method is called when the button is clicked"""
     anvil.server.call(
       "outline", 
@@ -22,16 +22,7 @@ class Form1(Form1Template):
 
     query_ra_step = app_tables.ra_steps.search()
     self.rpanel_ra_step.items = query_ra_step
-    self.rpanel_ra_step2 = query_ra_step
-
-  def button_1_copy_click(self, **event_args):
-    """This method is called when the button is clicked"""
-    query_gi_steps = app_tables.gi_steps.search()
-    self.rpanel_gi_steps.items = query_gi_steps
-    pass
 
     
-    
 
-    
 
