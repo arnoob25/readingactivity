9b8600529bf19b6890214f3b997eeb7d1aff9c35
muhammadarnobshikder@gmail.com <muhammadarnobshikder@gmail.com>
2023-08-29 09:36:53 +0000

    Edited MainPage

--- client_code/MainPage/__init__.py
+++ client_code/MainPage/__init__.py
@@ -19,6 +19,12 @@ class MainPage(MainPageTemplate):
     self.query_ra_steps = None
     self.data = {}
     self.question = None
+
+    # temp data storage
+    self.lo = ""
+    self.milestones = {}
+    self.gi_steps = {}
+    self.inquiries = {}
     
     # Any code you write here will run before the form opens.
     self.author_page1.visible = True
@@ -30,6 +36,21 @@ class MainPage(MainPageTemplate):
     # review
     self.curr_file = app_tables.files.get(title="Cardiac Cycle")
 
+  # ------ Server functions ------ 
+
+  # Task: Provide sample output
+  
+  def outline(self, ilo):
+    return milestones, lo, filename 
+
+  def gi(self, curr_step, lo, obj):
+    return steps
+
+  def inquiry(self, obj, gi_step):
+    return tool, prompt, options, assess
+  
+  # ------ helper functions ------ 
+  
   def reset(self):
     self.curr_file = None
     self.curr_ra_step = 1
@@ -75,27 +96,55 @@ class MainPage(MainPageTemplate):
         self.rpanel_options.items = self.question['options']
         self.title.text = f"Step {self.curr_ra_step} (question {self.curr_gi_step}/{len(self.data[self.curr_ra_step])}):"
 
+  # ------ event listeners ------
+
   def btn_gen_outline_click(self, **event_args):
     """This method is called when the button is clicked"""
 
+    # ------ basic ui logic ------
+    
     self.title.text = "Steps of the Rading Activity:"
     self.title.scroll_into_view()
     self.author_page1.visible = False
     self.author_page2.visible = True
     self.author_page2.scroll_into_view(smooth = False)
 
-    self.query_ra_steps = app_tables.ra_steps.search(file = self.curr_file)
+    # ------ making inference ------
     
+    ilo = self.tbox_ilo.text
+    self.milestones, self.lo, filename = self.outline(ilo)
+
+    # Task: have to crate new DB item with the filename
+    
+    # ------ DB query ------
+
+    # Task: I haven't yet saved the data into the DB, so the data must be loaded from the dictionary
+    
+    self.query_ra_steps = app_tables.ra_steps.search(file = self.curr_file)
     self.rpanel_ra_step.items = self.query_ra_steps
     self.rpanel_ra_step2.items = self.query_ra_steps
 
   def btn_gen_gi_click(self, **event_args):
     """This method is called when the button is clicked"""
 
+    # ------ basic ui logic ------
+
     self.title.text = "Steps with Guided Inquiries:"
     self.title.scroll_into_view()
     self.author_page2.visible = False
     self.author_page3.visible = True
+
+    # Task: Have to save the self.milestones data into the DB
+
+    # ------ making inference ------
+
+    for s in self.milestones:
+      serial = self.milestones[s][0]
+      objective = self.milestones[s][1]
+      list_gi_steps = gi(serial, self.lo, objective)
+      
+      # adding the gi_steps list to the dictionary
+      self.gi_steps[s] = list_gi_steps
 
   def btn_gen_question_click(self, **event_args):
     """This method is called when the button is clicked"""
