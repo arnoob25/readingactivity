7590daab4740f3eb72d19dccb4a18fc2fd6e9f9f
muhammadarnobshikder@gmail.com <muhammadarnobshikder@gmail.com>
2023-09-26 04:17:24 +0000

    Edited MainPage

--- client_code/MainPage/__init__.py
+++ client_code/MainPage/__init__.py
@@ -57,7 +57,8 @@ class MainPage(MainPageTemplate):
     self.inquiries = self.curr_file['inquiries']
     self.question = self.inquiries[0][0]
     self.title.text = f"Step {1} question: {1} of {len(self.inquiries[0])}"
-    self.interactive_component.html = server.call('format_html', self.question['code'])
+    alert(self.question['code'])
+    self.interactive_component.html = server.call('recover_html', self.question['code'])
     self.rtext_student_context.content = self.question['context']
     self.rtext_student_prompt.content = self.question['inquiry']
     self.rpanel_student_options.items = self.question['options']
