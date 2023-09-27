f4c53df8a478a951dd0f2f154a5d981cb87a921e
muhammadarnobshikder@gmail.com <muhammadarnobshikder@gmail.com>
2023-09-27 10:02:44 +0000

    Created MainPage.interactiveComponent; Edited settings, MainPage and test; Deleted MainPage.InteractiveComponent

--- client_code/MainPage/interactiveComponent/__init__.py
+++ client_code/MainPage/interactiveComponent/__init__.py
@@ -1,4 +1,4 @@
-from ._anvil_designer import InteractiveComponentTemplate
+from ._anvil_designer import interactiveComponentTemplate
 from anvil import *
 import anvil.server
 import anvil.google.auth, anvil.google.drive
@@ -8,7 +8,7 @@ import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
 
-class InteractiveComponent(InteractiveComponentTemplate):
+class interactiveComponent(interactiveComponentTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
